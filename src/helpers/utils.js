201c5f45570b9b3deb050bd795d6931c6d13c23e
Thales Casa Grande de Lima <thales-lima@live.com>
2025-03-22 22:31:53 -0300

    Minor fixes

---
+++ src/helpers/utils.js
@@ -0,0 +1,24 @@
+export function getTratamentoProgresso(data_inicio_tratamento, data_final_previsa) {
+    if (!data_inicio_tratamento || !data_final_previsa)
+        return '-';
+
+    const inicio = new Date(data_inicio_tratamento);
+    const termino = new Date(data_final_previsa);
+    const hoje = new Date();
+
+    if (hoje < inicio)
+        return 0.00;
+    if (hoje > termino)
+        return 100.00;
+
+    const duracaoTotal = termino.getTime() - inicio.getTime();
+    const duracaoAteHoje = hoje.getTime() - inicio.getTime();
+
+    const progresso = (duracaoAteHoje / duracaoTotal) * 100;
+
+    return parseFloat(progresso.toFixed(2));
+}
+
+export function capitalizeFirst(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
